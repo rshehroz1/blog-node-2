70cdecb045453b47f51d5193f6dbdea125f727ef
Shehroz <116956168+rshehroz1@users.noreply.github.com>
2023-02-07 20:51:35 +0500

    test
    test mongoodb

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
 const expressEdge = require("express-edge")
+const mongoose = require("mongoose")
 const path = require("path")
 
+mongoose.connect("mongodb+srv://Shehroz:dadam123@cluster0.mxtjdya.mongodb.net/merigram")
 
 app.use(express.static("public"))
 app.use(expressEdge.engine)
---
+++ test.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose")
+
+const Post = require("./models/post")
+
+mongoose.connect("mongodb+srv://Shehroz:dadam123@cluster0.mxtjdya.mongodb.net/merigram")
